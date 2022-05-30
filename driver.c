afb9fac3e1c2927ffc24977da7a131e607a91c2f
Neo Bethuel Morakile <morakileneo@gmail.com>
2022-05-30 15:05:29 +0200

    driver.c Task

--- driver.c
+++ driver.c
@@ -6,7 +6,7 @@
  * Description: the function loops through the structs
  * selector[] ment of the struct.
  * Return: a pointer
- * structype selector - Struct
+ * structype selector - Struct.
  */
 int (*driver(char *format))(char *format, va_list)
 {
